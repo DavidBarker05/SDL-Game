41c931bca27a009ca7e2b44edb2a9585ed77f647
DavidBarker05 <davidbarker9611@gmail.com>
2026-02-25 14:36:20 +0200

    Moved run logic from main to game

--- SDL-Game/src/Main.cpp
+++ SDL-Game/src/Main.cpp
@@ -1,33 +1,7 @@
-#include <SDL.h>
+#include "Game.h"
 
-// SDL forces me to write main like this for some reason
-int main(int argc, char** argv)
+int main()
 {
-	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-	SDL_Window* window;
-	SDL_Renderer* renderer;
-	SDL_Event e;
-	bool running = true;
-
-	window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
-	renderer = SDL_CreateRenderer(window, -1, 0);
-
-	while (running)
-	{
-		while (SDL_PollEvent(&e))
-		{
-			if (e.type == SDL_QUIT) running = false;
-		}
-
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-
-		SDL_RenderPresent(renderer);
-	}
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-	return 0;
+	Game game("Title", 640, 480);
+	game.Run();
 }

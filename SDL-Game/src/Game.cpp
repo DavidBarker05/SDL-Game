8f954cca0b7a65c5432cc3503033ac067c215eb5
DavidBarker05 <davidbarker9611@gmail.com>
2026-03-07 18:40:10 +0200

    added delta time I think

--- SDL-Game/src/Game.cpp
+++ SDL-Game/src/Game.cpp
@@ -22,12 +22,15 @@ void Game::Run()
 	m_bGameIsRunning = TRUE;
 	while (m_bGameIsRunning)
 	{
+		static INT64 lastTick = 0i64;
 		PollStatus status = m_EventManager.PollEvents();
 		if (status == PollStatus::eQUIT)
 		{
 			m_bGameIsRunning = FALSE;
 			return;
 		}
+		m_DeltaTime = (SDL_GetTicks64() - lastTick) / 1000.0f;
+		lastTick = SDL_GetTicks64();
 		m_GameRenderer.Render();
 	}
 }

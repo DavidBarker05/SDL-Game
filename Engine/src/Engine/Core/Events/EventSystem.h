adc397850b36ddd7940dcac54b0887ad4430ad76
DavidBarker05 <davidbarker9611@gmail.com>
2026-05-13 03:35:20 +0200

    Added comments for EventSystem header

--- Engine/src/Engine/Core/Events/EventSystem.h
+++ Engine/src/Engine/Core/Events/EventSystem.h
@@ -5,12 +5,15 @@
 class EventSystem
 {
 public:
-	static constexpr UINT32 Quit = 0;
-	static constexpr UINT32 AllEventsPolled = 1;
+	static constexpr UINT32 Quit = 0; // Code returned by PollEvents() when the game is closed
+	static constexpr UINT32 AllEventsPolled = 1; // Code returned by PollEvents() when all events have been polled
 
 public:
+	// Initialise the event system. Returns true of success and false on fail
 	static bool Init();
+	// Shutdown the event system
 	static void Shutdown();
 
+	// Poll all the events
 	static UINT32 PollEvents();
 };

fbda11df40ad0e55bc3a9233779107bd78650ebd
DavidBarker05 <davidbarker9611@gmail.com>
2026-05-19 23:42:12 +0200

    finished up file system

--- Engine/src/Engine/Core/Files/FileSystem.cpp
+++ Engine/src/Engine/Core/Files/FileSystem.cpp
@@ -2,15 +2,22 @@
 #define WINDOWS 1
 #endif
 
+// I've noticed I've made my life much harder by not using c++17 but oh well
+// ig this gives me some practice
 #include "FileSystem.h"
 #include "Logging/Log.h"
-#include <cstdlib>
-#include <iostream>
-#include <string>
 #include "config.h"
+#include <codecvt>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <sys/stat.h> // POSIX header but is included with basic functionality on Windows :)
+#include <sys/types.h>
 #if WINDOWS
-#include <windows.h>
 #include <ShlObj_core.h>
+#include <windows.h>
 #define PATH_MAX MAX_PATH
 #define SEPARATOR '\\'
 #define SEPARATOR_STR "\\"
@@ -18,215 +25,420 @@
 #include <codecvt>
 #define buffer_t wchar_t
 #define string_t std::wstring
+#define FROM_STRING(string) std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(string)
 #define TO_STRING(wstring) std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(wstring)
 #else
 #define buffer_t char
-#define string_t std::string
-#define TO_CHARBUFF(src, dst, size) std::memcpy(dst, src, size)
+#define string_t STRING
+#define FROM_STRING(string) STRING(string)
 #define TO_STRING(string) string
 #endif
 #else // Linux, MacOS, etc.
-#include <unistd.h>
 #include <limits.h>
+#include <unistd.h>
 #define SEPARATOR '/'
 #define SEPARATOR_STR "/"
 #define buffer_t char
-#define string_t std::string
+#define string_t STRING
+#define FROM_STRING(string) STRING(string)
 #define TO_STRING(string) string
 #endif
 
 static bool s_bInitialised = false;
 
-static CSTRING s_ExecutablePath;
-static CSTRING s_PersistentDataPath;
-static CSTRING s_TemporaryDataPath;
+static STRING s_ExecutablePath;
+static STRING s_PersistentDataPath;
+static STRING s_TemporaryDataPath;
 
-SIZE_T FileSystem::PathMax()
-{
-    return PATH_MAX;
-}
-
-CSTRING FileSystem::ExecutablePath()
-{
-    return s_ExecutablePath;
-}
-
-CSTRING FileSystem::PersistentDataPath()
-{
-    return s_PersistentDataPath;
-}
-
-CSTRING FileSystem::TemporaryDataPath()
-{
-    return s_TemporaryDataPath;
-}
-
-bool FileSystem::IsInitialized()
-{
-    return s_bInitialised;
-}
+bool FileSystem::IsInitialized() { return s_bInitialised; }
 
 static void FindExecutablePath()
 {
-    buffer_t pathBuffer[PATH_MAX];
-    std::string output;
+	buffer_t pathBuffer[PATH_MAX];
 #if WINDOWS
 	GetModuleFileName(nullptr, pathBuffer, PATH_MAX);
 #else
 	ssize_t len = readlink("/proc/self/exe", pathBuffer, sizeof(pathBuffer) - 1);
 	if (len != -1) pathBuffer[len] = '\0';
 #endif
-    output = TO_STRING(string_t(pathBuffer));
-	std::size_t lastPos = output.find_last_of(SEPARATOR);
-	output = output.substr(0, lastPos);
-    std::strcpy(const_cast<char*>(s_ExecutablePath), output.c_str());
+	s_ExecutablePath = FileSystem::GetDirectory(TO_STRING(string_t(pathBuffer)));
 }
 
 static void FindPersistentDataPath()
 {
-    char pathBuffer[PATH_MAX];
+	char pathBuffer[PATH_MAX];
 #if WINDOWS
-    PWSTR path = nullptr;
-    HRESULT result = SHGetKnownFolderPath(FOLDERID_LocalAppDataLow, 0, nullptr, &path);
-    if (SUCCEEDED(result))
-    {
-        std::wcstombs(pathBuffer, path, PATH_MAX);
-        CoTaskMemFree(path);
-    }
-    if (strlen(pathBuffer) == 0) // Failed to get LocalLow
-    {
-        CSTRING userPath = std::getenv("USERPROFILE");
-        if (strlen(userPath) > 0)
-        {
-            FileSystem::Combine(userPath, "AppData", pathBuffer);
-            FileSystem::Combine(pathBuffer, "LocalLow", pathBuffer);
-        }
-    }
-    if (strlen(pathBuffer) == 0) // Still failed to find
-    {
-        std::strcpy(pathBuffer, "C:");
-        FileSystem::Combine(pathBuffer, "Users", pathBuffer);
-        FileSystem::Combine(pathBuffer, "Default", pathBuffer);
-        FileSystem::Combine(pathBuffer, "AppData", pathBuffer);
-        FileSystem::Combine(pathBuffer, "Local", pathBuffer);
-    }
-    FileSystem::Combine(pathBuffer, COMPANY_NAME, pathBuffer);
-    FileSystem::Combine(pathBuffer, PRODUCT_NAME, pathBuffer);
+	PWSTR path = nullptr;
+	HRESULT result = SHGetKnownFolderPath(FOLDERID_LocalAppDataLow, 0, nullptr, &path);
+	if (SUCCEEDED(result))
+	{
+		std::wcstombs(pathBuffer, path, PATH_MAX);
+		CoTaskMemFree(path);
+	}
+	if (strlen(pathBuffer) == 0) // Failed to get LocalLow
+	{
+		CSTRING userPath = std::getenv("USERPROFILE");
+		if (strlen(userPath) > 0)
+		{
+			FileSystem::Combine(userPath, "AppData", pathBuffer);
+			FileSystem::Combine(pathBuffer, "LocalLow", pathBuffer);
+		}
+	}
+	if (strlen(pathBuffer) == 0) // Still failed to find
+	{
+		std::strcpy(pathBuffer, "C:");
+		FileSystem::Combine(pathBuffer, "Users", pathBuffer);
+		FileSystem::Combine(pathBuffer, "Default", pathBuffer);
+		FileSystem::Combine(pathBuffer, "AppData", pathBuffer);
+		FileSystem::Combine(pathBuffer, "Local", pathBuffer);
+	}
+	FileSystem::Combine(pathBuffer, COMPANY_NAME, pathBuffer);
+	FileSystem::Combine(pathBuffer, PRODUCT_NAME, pathBuffer);
 #elif defined(__APPLE__)
-    CSTRING home = std::getenv("HOME");
-    FileSystem::Combine(home, "Library", pathBuffer);
-    FileSystem::Combine(pathBuffer, "Application Support", pathBuffer);
-    FileSystem::Combine(pathBuffer, "com", pathBuffer);
-    std::strcpy(pathBuffer + strlen(pathBuffer), ".");
-    std::strcpy(pathBuffer + strlen(pathBuffer), COMPANY_NAME);
-    std::strcpy(pathBuffer + strlen(pathBuffer), ".");
-    std::strcpy(pathBuffer + strlen(pathBuffer), PRODUCT_NAME);
+	CSTRING home = std::getenv("HOME");
+	FileSystem::Combine(home, "Library", pathBuffer);
+	FileSystem::Combine(pathBuffer, "Application Support", pathBuffer);
+	FileSystem::Combine(pathBuffer, "com", pathBuffer);
+	std::strcpy(pathBuffer + strlen(pathBuffer), ".");
+	std::strcpy(pathBuffer + strlen(pathBuffer), COMPANY_NAME);
+	std::strcpy(pathBuffer + strlen(pathBuffer), ".");
+	std::strcpy(pathBuffer + strlen(pathBuffer), PRODUCT_NAME);
 #else // Linux
-    CSTRING configHome = std::getenv("XDG_CONFIG_HOME");
-    std::strcpy(pathBuffer, configHome);
-    if (strlen(pathBuffer) == 0)
-    {
-        CSTRING home = std::getenv("HOME");
-        std::strcpy(pathBuffer, home);
-    }
-    FileSystem::Combine(pathBuffer, COMPANY_NAME, pathBuffer);
-    FileSystem::Combine(pathBuffer, PRODUCT_NAME, pathBuffer);
+	CSTRING configHome = std::getenv("XDG_CONFIG_HOME");
+	std::strcpy(pathBuffer, configHome);
+	if (strlen(pathBuffer) == 0)
+	{
+		CSTRING home = std::getenv("HOME");
+		std::strcpy(pathBuffer, home);
+	}
+	FileSystem::Combine(pathBuffer, COMPANY_NAME, pathBuffer);
+	FileSystem::Combine(pathBuffer, PRODUCT_NAME, pathBuffer);
 #endif
-    std::strcpy(const_cast<char*>(s_PersistentDataPath), pathBuffer);
+	s_PersistentDataPath = pathBuffer;
 }
 
 static void FindTemporaryDataPath()
 {
-    char pathBuffer[PATH_MAX];
+	char pathBuffer[PATH_MAX];
 #if WINDOWS
-    CSTRING localPath = std::getenv("LOCALAPPDATA");
-    std::strcpy(pathBuffer, localPath);
-    if (strlen(pathBuffer) == 0) // Failed to get LocalLow
-    {
-        CSTRING userPath = std::getenv("USERPROFILE");
-        if (strlen(userPath) > 0)
-        {
-            FileSystem::Combine(userPath, "AppData", pathBuffer);
-            FileSystem::Combine(pathBuffer, "Local", pathBuffer);
-        }
-    }
-    if (strlen(pathBuffer) == 0) // Still failed to find
-    {
-        std::strcpy(pathBuffer, "C:");
-        FileSystem::Combine(pathBuffer, "Users", pathBuffer);
-        FileSystem::Combine(pathBuffer, "Default", pathBuffer);
-        FileSystem::Combine(pathBuffer, "AppData", pathBuffer);
-        FileSystem::Combine(pathBuffer, "Local", pathBuffer);
-    }
-    FileSystem::Combine(pathBuffer, "Temp", pathBuffer);
-    FileSystem::Combine(pathBuffer, COMPANY_NAME, pathBuffer);
-    FileSystem::Combine(pathBuffer, PRODUCT_NAME, pathBuffer);
+	CSTRING localPath = std::getenv("LOCALAPPDATA");
+	std::strcpy(pathBuffer, localPath);
+	if (strlen(pathBuffer) == 0) // Failed to get LocalLow
+	{
+		CSTRING userPath = std::getenv("USERPROFILE");
+		if (strlen(userPath) > 0)
+		{
+			FileSystem::Combine(userPath, "AppData", pathBuffer);
+			FileSystem::Combine(pathBuffer, "Local", pathBuffer);
+		}
+	}
+	if (strlen(pathBuffer) == 0) // Still failed to find
+	{
+		std::strcpy(pathBuffer, "C:");
+		FileSystem::Combine(pathBuffer, "Users", pathBuffer);
+		FileSystem::Combine(pathBuffer, "Default", pathBuffer);
+		FileSystem::Combine(pathBuffer, "AppData", pathBuffer);
+		FileSystem::Combine(pathBuffer, "Local", pathBuffer);
+	}
+	FileSystem::Combine(pathBuffer, "Temp", pathBuffer);
+	FileSystem::Combine(pathBuffer, COMPANY_NAME, pathBuffer);
+	FileSystem::Combine(pathBuffer, PRODUCT_NAME, pathBuffer);
 #elif defined(__APPLE__)
-    CSTRING home = std::getenv("HOME");
-    FileSystem::Combine(home, "Library", pathBuffer);
-    FileSystem::Combine(pathBuffer, "Caches", pathBuffer);
-    FileSystem::Combine(pathBuffer, "com", pathBuffer);
-    std::strcpy(pathBuffer + strlen(pathBuffer), ".");
-    std::strcpy(pathBuffer + strlen(pathBuffer), COMPANY_NAME);
-    std::strcpy(pathBuffer + strlen(pathBuffer), ".");
-    std::strcpy(pathBuffer + strlen(pathBuffer), PRODUCT_NAME);
+	CSTRING home = std::getenv("HOME");
+	FileSystem::Combine(home, "Library", pathBuffer);
+	FileSystem::Combine(pathBuffer, "Caches", pathBuffer);
+	FileSystem::Combine(pathBuffer, "com", pathBuffer);
+	std::strcpy(pathBuffer + strlen(pathBuffer), ".");
+	std::strcpy(pathBuffer + strlen(pathBuffer), COMPANY_NAME);
+	std::strcpy(pathBuffer + strlen(pathBuffer), ".");
+	std::strcpy(pathBuffer + strlen(pathBuffer), PRODUCT_NAME);
 #else // Linux
-    CSTRING tmp = std::getenv("TMPDIR");
-    std::strcpy(pathBuffer, tmp);
-    if (strlen(pathBuffer) == 0)
-    {
-        FileSystem::Combine("", "var", pathBuffer);
-        FileSystem::Combine(pathBuffer, "tmp", pathBuffer);
-    }
-    FileSystem::Combine(pathBuffer, COMPANY_NAME, pathBuffer);
-    FileSystem::Combine(pathBuffer, PRODUCT_NAME, pathBuffer);
+	CSTRING tmp = std::getenv("TMPDIR");
+	std::strcpy(pathBuffer, tmp);
+	if (strlen(pathBuffer) == 0)
+	{
+		FileSystem::Combine("", "var", pathBuffer);
+		FileSystem::Combine(pathBuffer, "tmp", pathBuffer);
+	}
+	FileSystem::Combine(pathBuffer, COMPANY_NAME, pathBuffer);
+	FileSystem::Combine(pathBuffer, PRODUCT_NAME, pathBuffer);
 #endif
-    std::strcpy(const_cast<char*>(s_TemporaryDataPath), pathBuffer);
+	s_TemporaryDataPath = pathBuffer;
 }
 
 void FileSystem::Init()
 {
-    s_ExecutablePath = (CSTRING)alloca(PATH_MAX);
-    s_PersistentDataPath = (CSTRING)alloca(PATH_MAX);
-    s_TemporaryDataPath = (CSTRING)alloca(PATH_MAX);
-    FindExecutablePath();
-    FindPersistentDataPath();
-    FindTemporaryDataPath();
-    if (Logger::IsInitialized())
-    {
-        LOG_INFO("%s", s_TemporaryDataPath);
-        LOG_INFO("Initialised the file system");
-    }
-    s_bInitialised = true;
+	FindExecutablePath();
+	FindPersistentDataPath();
+	FindTemporaryDataPath();
+	if (Logger::IsInitialized()) LOG_INFO("Initialised the file system");
+	s_bInitialised = true;
 }
 
-void FileSystem::Combine(CSTRING pathLeft, CSTRING pathRight, char* buffer)
+SIZE_T FileSystem::PathMax() { return PATH_MAX; }
+
+CSTRING FileSystem::ExecutablePath() { return s_ExecutablePath.c_str(); }
+
+CSTRING FileSystem::PersistentDataPath() { return s_PersistentDataPath.c_str(); }
+
+CSTRING FileSystem::TemporaryDataPath() { return s_TemporaryDataPath.c_str(); }
+
+void FileSystem::Combine(CSTRING pathLeft, CSTRING pathRight, char* buffer, SIZE_T bufferSize)
 {
-    std::strcpy(buffer, pathLeft);
-    std::strcpy(buffer + strlen(buffer), SEPARATOR_STR);
-    std::strcpy(buffer + strlen(buffer), pathRight);
+	std::strncpy(buffer, pathLeft, bufferSize);
+	std::strncpy(buffer + strlen(buffer), SEPARATOR_STR, bufferSize - strlen(buffer));
+	std::strncpy(buffer + strlen(buffer), pathRight, bufferSize - strlen(buffer));
 }
 
-void FileSystem::Combine(CSTRING pathLeft, CSTRING pathRight, char** pBuffer)
+void FileSystem::Combine(const STRING& pathLeft, CSTRING pathRight, char* buffer, SIZE_T bufferSize)
 {
-    *pBuffer = (char*)malloc(PATH_MAX);
-    std::strcpy(*pBuffer, pathLeft);
-    std::strcpy(*pBuffer + strlen(*pBuffer), SEPARATOR_STR);
-    std::strcpy(*pBuffer + strlen(*pBuffer), pathRight);
+	Combine(pathLeft.c_str(), pathRight, buffer, bufferSize);
 }
 
-bool FileSystem::ReadFile(CSTRING path, char* output)
+void FileSystem::Combine(CSTRING pathLeft, const STRING& pathRight, char* buffer, SIZE_T bufferSize)
 {
-    // TODO Read file
-    return false;
+	Combine(pathLeft, pathRight.c_str(), buffer, bufferSize);
+}
+
+void FileSystem::Combine(const STRING& pathLeft, const STRING& pathRight, char* buffer,
+						 SIZE_T bufferSize)
+{
+	Combine(pathLeft.c_str(), pathRight.c_str(), buffer, bufferSize);
+}
+
+STRING FileSystem::Combine(CSTRING pathLeft, CSTRING pathRight)
+{
+	CSTRING buffer = (CSTRING)alloca(PATH_MAX);
+	Combine(pathLeft, pathRight, const_cast<char*>(buffer));
+	return STRING(buffer);
+}
+
+STRING FileSystem::Combine(const STRING& pathLeft, CSTRING pathRight)
+{
+	return Combine(pathLeft.c_str(), pathRight);
+}
+
+STRING FileSystem::Combine(CSTRING pathLeft, const STRING& pathRight)
+{
+	return Combine(pathLeft, pathRight.c_str());
+}
+
+STRING FileSystem::Combine(const STRING& pathLeft, const STRING& pathRight)
+{
+	return Combine(pathLeft.c_str(), pathRight.c_str());
+}
+
+void FileSystem::Combine(CSTRING pathLeft, CSTRING pathRight, STRING& result)
+{
+	result = Combine(pathLeft, pathRight);
+}
+
+void FileSystem::Combine(STRING pathLeft, CSTRING pathRight, STRING& result)
+{
+	result = Combine(pathLeft, pathRight);
+}
+
+void FileSystem::Combine(CSTRING pathLeft, STRING pathRight, STRING& result)
+{
+	result = Combine(pathLeft, pathRight);
+}
+
+void FileSystem::Combine(STRING pathLeft, STRING pathRight, STRING& result)
+{
+	result = Combine(pathLeft, pathRight);
+}
+
+void FileSystem::GetFilePart(CSTRING path, char* buffer, SIZE_T bufferSize)
+{
+	CSTRING pLastSeparator = std::strrchr(path, SEPARATOR);
+	pLastSeparator ? std::strncpy(buffer, &path[pLastSeparator - path + 1], bufferSize)
+				   : std::strncpy(buffer, path, bufferSize);
+}
+
+void FileSystem::GetFilePart(const STRING& path, char* buffer, SIZE_T bufferSize)
+{
+	GetFilePart(path.c_str(), buffer, bufferSize);
+}
+
+STRING FileSystem::GetFilePart(CSTRING path)
+{
+	STRING output(path);
+	output = output.substr(output.find_last_of(SEPARATOR_STR) + 1);
+	return output;
+}
+
+STRING FileSystem::GetFilePart(const STRING& path)
+{
+	STRING output(path);
+	output = output.substr(output.find_last_of(SEPARATOR_STR) + 1);
+	return output;
+}
+
+void FileSystem::GetFilePart(CSTRING path, STRING& result)
+{
+	result = path;
+	result = result.substr(result.find_last_of(SEPARATOR_STR) + 1);
+}
+
+void FileSystem::GetFilePart(const STRING& path, STRING& result)
+{
+	result = path;
+	result = result.substr(result.find_last_of(SEPARATOR_STR) + 1);
+}
+
+void FileSystem::GetDirectory(CSTRING path, char* buffer, SIZE_T bufferSize)
+{
+	CSTRING pLastSeparator = std::strrchr(path, SEPARATOR);
+	SIZE_T count = pLastSeparator
+					   ? (pLastSeparator - path < bufferSize ? pLastSeparator - path : bufferSize)
+					   : bufferSize;
+	std::strncpy(buffer, path, count);
+}
+
+void FileSystem::GetDirectory(const STRING& path, char* buffer, SIZE_T bufferSize)
+{
+	GetDirectory(path.c_str(), buffer, bufferSize);
+}
+
+STRING FileSystem::GetDirectory(CSTRING path)
+{
+	STRING output(path);
+	output = output.substr(0, output.find_last_of(SEPARATOR_STR));
+	return output;
+}
+
+STRING FileSystem::GetDirectory(const STRING& path)
+{
+	STRING output(path);
+	output = output.substr(0, output.find_last_of(SEPARATOR_STR));
+	return output;
+}
+
+void FileSystem::GetDirectory(CSTRING path, STRING& result) { result = GetDirectory(path); }
+
+void FileSystem::GetDirectory(const STRING& path, STRING& result) { result = GetDirectory(path); }
+
+bool FileSystem::FileExists(CSTRING file) { return std::ifstream(file).good(); }
+
+bool FileSystem::FileExists(const STRING& file) { return std::ifstream(file).good(); }
+
+bool FileSystem::DirectoryExists(CSTRING directory)
+{
+	struct stat info;
+	return (!stat(directory, &info)) && (info.st_mode & S_IFDIR);
+}
+
+bool FileSystem::DirectoryExists(const STRING& directory)
+{
+	return DirectoryExists(directory.c_str());
+}
+
+void FileSystem::MakeDirectory(CSTRING directory)
+{
+	// Sadly no universal way to do this pre c++17 :(
+#if WINDOWS
+	CreateDirectory(FROM_STRING(directory).c_str(), nullptr);
+#else
+	mkdir(directory, 0777);
+#endif
+}
+
+void FileSystem::MakeDirectory(const STRING& directory) { MakeDirectory(directory.c_str()); }
+
+bool FileSystem::ReadFile(CSTRING path, char* output, SIZE_T bufferSize)
+{
+	if (FILE* pFile = fopen(path, "r"))
+	{
+		std::fread(output, sizeof(char), bufferSize, pFile);
+		fclose(pFile);
+		return true;
+	}
+	return false;
+}
+
+bool FileSystem::ReadFile(CSTRING path, STRING& output)
+{
+	std::ifstream file(path);
+	if (file.is_open())
+	{
+		std::stringstream buffer;
+		buffer << file.rdbuf();
+		output = buffer.str();
+		return true;
+	}
+	return false;
+}
+
+bool FileSystem::ReadFile(const STRING& path, char* output, SIZE_T bufferSize)
+{
+	return ReadFile(path.c_str(), output, bufferSize);
+}
+
+bool FileSystem::ReadFile(const STRING& path, STRING& output)
+{
+	return ReadFile(path.c_str(), output);
 }
 
 void FileSystem::WriteFile(CSTRING path, CSTRING contents)
 {
-    // TODO Write to file
+	STRING dir = GetDirectory(path);
+	if (!DirectoryExists(dir)) MakeDirectory(dir);
+	std::ofstream file(path, std::ios::trunc); // Explicit truncation
+	file << contents;
 }
 
-void FileSystem::AppendLine(CSTRING path, CSTRING contents)
+void FileSystem::WriteFile(CSTRING path, const STRING& contents)
 {
-    // TODO Append line to file
+	STRING dir = GetDirectory(path);
+	if (!DirectoryExists(dir)) MakeDirectory(dir);
+	std::ofstream file(path, std::ios::trunc); // Explicit truncation
+	file << contents;
+}
+
+void FileSystem::WriteFile(const STRING& path, CSTRING contents)
+{
+	STRING dir = GetDirectory(path);
+	if (!DirectoryExists(dir)) MakeDirectory(dir);
+	std::ofstream file(path, std::ios::trunc); // Explicit truncation
+	file << contents;
+}
+
+void FileSystem::WriteFile(const STRING& path, const STRING& contents)
+{
+	STRING dir = GetDirectory(path);
+	if (!DirectoryExists(dir)) MakeDirectory(dir);
+	std::ofstream file(path, std::ios::trunc); // Explicit truncation
+	file << contents;
+}
+
+void FileSystem::AppendFile(CSTRING path, CSTRING contents)
+{
+	STRING dir = GetDirectory(path);
+	if (!DirectoryExists(dir)) MakeDirectory(dir);
+	std::ofstream file(path, std::ios::app);
+	file << contents;
+}
+
+void FileSystem::AppendFile(CSTRING path, const STRING& contents)
+{
+	STRING dir = GetDirectory(path);
+	if (!DirectoryExists(dir)) MakeDirectory(dir);
+	std::ofstream file(path, std::ios::app);
+	file << contents;
+}
+
+void FileSystem::AppendFile(const STRING& path, CSTRING contents)
+{
+	STRING dir = GetDirectory(path);
+	if (!DirectoryExists(dir)) MakeDirectory(dir);
+	std::ofstream file(path, std::ios::app);
+	file << contents;
+}
+
+void FileSystem::AppendFile(const STRING& path, const STRING& contents)
+{
+	STRING dir = GetDirectory(path);
+	if (!DirectoryExists(dir)) MakeDirectory(dir);
+	std::ofstream file(path, std::ios::app);
+	file << contents;
 }
--- Engine/src/Engine/Core/Logging/Log.cpp
+++ Engine/src/Engine/Core/Logging/Log.cpp
@@ -1,16 +1,17 @@
 #include "Log.h"
-#include <SDL3/SDL_log.h>
-#include <iostream>
-#include <iomanip>
-#include <ctime> // Because displaying a decent time with chrono is a bitch
-#include <cpptrace/basic.hpp>
-#include "Math/Math.h"
 #include "Files/FileSystem.h"
+#include "Math/Math.h"
+#include <SDL3/SDL_log.h>
+#include <cpptrace/basic.hpp>
+#include <ctime> // Because displaying a decent time with chrono is a bitch
+#include <iomanip>
+#include <iostream>
 #include <sstream>
 
 #ifdef _MSC_VER
-#pragma warning(disable : 4996) // Because MSVC won't let you compile if you use ctime stuff. Even tho this is a warning? Like wtf Microsoft?
-#endif // _MSC_VER
+#pragma warning(disable : 4996) // Because MSVC won't let you compile if you use ctime stuff. Even
+								// tho this is a warning? Like wtf Microsoft?
+#endif
 
 #define DEFAULT_COLOUR "\33[0;0m"
 #define GREEN "\33[0;92m"
@@ -21,9 +22,10 @@
 static bool s_bInitialised;
 static INT8 s_LastLevel;
 static INT8 s_CurrentLevel;
-static CSTRING s_LogFile = "\0";
+static STRING s_LogFile;
 
-// This is my understanding of how SDL does theirs, but like it's a lot shorter idk if I'm doing this in an unoptimised way but oh well
+// This is my understanding of how SDL does theirs, but like it's a lot shorter idk if I'm doing
+// this in an unoptimised way but oh well
 CSTRING FormatMessage(CSTRING format, va_list ap)
 {
 	char buffer[256];
@@ -44,31 +46,31 @@ void LogMessage(LogLevel logLevel, CSTRING message)
 	bool bStackTrace = false;
 	switch (logLevel)
 	{
-		case LogLevel::eTRACE:
-			type = "TRACE";
-			colour = DEFAULT_COLOUR;
-			break;
-		case LogLevel::eINFO:
-			type = "INFO";
-			colour = GREEN;
-			break;
-		case LogLevel::eWARN:
-			type = "WARNING";
-			colour = YELLOW;
-			bStackTrace = true;
-			break;
-		case LogLevel::eERROR:
-			type = "ERROR";
-			colour = RED;
-			bStackTrace = true;
-			break;
-		case LogLevel::eFATAL:
-			type = "FATAL";
-			colour = DEEPER_RED;
-			bStackTrace = true;
-			break;
-		default:
-			return;
+	case LogLevel::eTRACE:
+		type = "TRACE";
+		colour = DEFAULT_COLOUR;
+		break;
+	case LogLevel::eINFO:
+		type = "INFO";
+		colour = GREEN;
+		break;
+	case LogLevel::eWARN:
+		type = "WARNING";
+		colour = YELLOW;
+		bStackTrace = true;
+		break;
+	case LogLevel::eERROR:
+		type = "ERROR";
+		colour = RED;
+		bStackTrace = true;
+		break;
+	case LogLevel::eFATAL:
+		type = "FATAL";
+		colour = DEEPER_RED;
+		bStackTrace = true;
+		break;
+	default:
+		return;
 	}
 	std::stringstream output;
 	std::time_t now = std::time(nullptr);
@@ -79,23 +81,28 @@ void LogMessage(LogLevel logLevel, CSTRING message)
 		auto stackTrace = cpptrace::generate_trace(1); // 1 skips the current function
 		for (auto it = stackTrace.cbegin(); it < stackTrace.cend(); ++it)
 		{
-			if (it->symbol.substr(0, strlen("LogOverrideFunction")) == "LogOverrideFunction") continue; // If rename the function don't forget to update string
-			if (it->symbol.substr(0, strlen("SDL_")) == "SDL_") continue; // SDL better not change their naming convention
+			if (it->symbol.substr(0, strlen("LogOverrideFunction")) == "LogOverrideFunction")
+				continue; // If rename the function don't forget to update string
+			if (it->symbol.substr(0, strlen("SDL_")) == "SDL_")
+				continue; // SDL better not change their naming convention
 			stackMessage += it->filename.empty() ? it->symbol : it->filename;
 			if (it->line.has_value()) stackMessage += " LINE " + std::to_string(it->line.value());
 			break;
 		}
 	}
-	output << type << ": [" << std::put_time(pLocalTime, "%H:%M:%S") << "] " << message << stackMessage;
+	output << type << ": [" << std::put_time(pLocalTime, "%H:%M:%S") << "] " << message
+		   << stackMessage;
 	std::cout << colour << output.str() << DEFAULT_COLOUR << "\n\n";
 	if (!FileSystem::IsInitialized()) return;
-	if (strlen(s_LogFile) == 0)
+	if (s_LogFile.empty())
 	{
-		s_LogFile = (CSTRING)alloca(FileSystem::PathMax());
-		FileSystem::Combine(FileSystem::PersistentDataPath(), "log_output.txt", const_cast<char*>(s_LogFile));
+		FileSystem::Combine(FileSystem::PersistentDataPath(), "log_output.txt", s_LogFile);
+		std::stringstream dateStream;
+		dateStream << "Log Creation Date: " << std::put_time(pLocalTime, "%x") << "\n\n";
+		FileSystem::WriteFile(s_LogFile, dateStream.str());
 	}
-	FileSystem::AppendLine(s_LogFile, output.str().c_str());
-	FileSystem::AppendLine(s_LogFile, "\n");
+	FileSystem::AppendFile(s_LogFile, output.str());
+	FileSystem::AppendFile(s_LogFile, "\n\n");
 }
 
 // Function to override SDL's log function, that way, even if someone calls SDL_Log() then
@@ -105,37 +112,34 @@ void LogOverrideFunction(void* userData, INT32 category, SDL_LogPriority priorit
 	LogLevel level;
 	switch (priority)
 	{
-		case SDL_LOG_PRIORITY_VERBOSE:
-			level = LogLevel::eTRACE;
-			break;
-		case SDL_LOG_PRIORITY_INFO:
-			level = LogLevel::eINFO;
-			break;
-		case SDL_LOG_PRIORITY_WARN:
-			level = LogLevel::eWARN;
-			break;
-		case SDL_LOG_PRIORITY_ERROR:
-			level = LogLevel::eERROR;
-			break;
-		case SDL_LOG_PRIORITY_CRITICAL:
-			level = LogLevel::eFATAL;
-			break;
-		default:
-			return;
+	case SDL_LOG_PRIORITY_VERBOSE:
+		level = LogLevel::eTRACE;
+		break;
+	case SDL_LOG_PRIORITY_INFO:
+		level = LogLevel::eINFO;
+		break;
+	case SDL_LOG_PRIORITY_WARN:
+		level = LogLevel::eWARN;
+		break;
+	case SDL_LOG_PRIORITY_ERROR:
+		level = LogLevel::eERROR;
+		break;
+	case SDL_LOG_PRIORITY_CRITICAL:
+		level = LogLevel::eFATAL;
+		break;
+	default:
+		return;
 	}
 	if (s_CurrentLevel < (INT8)level) return;
 	LogMessage(level, message);
 }
 
-bool Logger::IsInitialized()
-{
-    return s_bInitialised;
-}
+bool Logger::IsInitialized() { return s_bInitialised; }
 
 void Logger::Init()
 {
 #ifdef LOG_LEVEL_TRACE
-	s_LastLevel = (INT8)LogLevel::eTRACE;	
+	s_LastLevel = (INT8)LogLevel::eTRACE;
 	s_CurrentLevel = (INT8)LogLevel::eTRACE;
 #elifdef LOG_LEVEL_INFO
 	s_LastLevel = (INT8)LogLevel::eINFO;
@@ -150,13 +154,13 @@ void Logger::Init()
 	s_LastLevel = (INT8)LogLevel::eFATAL;
 	s_CurrentLevel = (INT8)LogLevel::eFATAL;
 #elifdef LOG_LEVEL_NONE
-	s_LastLevel = -1;	
+	s_LastLevel = -1;
 	s_CurrentLevel = -1;
 #else
 	s_LastLevel = (INT8)LogLevel::eTRACE;
 	s_CurrentLevel = (INT8)LogLevel::eTRACE;
 #endif
-	SDL_SetLogOutputFunction((SDL_LogOutputFunction) LogOverrideFunction, nullptr);
+	SDL_SetLogOutputFunction((SDL_LogOutputFunction)LogOverrideFunction, nullptr);
 	s_bInitialised = true;
 	LOG_INFO("Initialised the logging system");
 }
